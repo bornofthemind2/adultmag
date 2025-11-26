bf14b9cea7963b31766a06879e41dcf456276fac
bornofthemind2 <ewellwear@gmail.com>
2025-11-26 05:51:46 -0500

    Refactor Vite config with environment and optimizations
    Updated Vite configuration to include environment variables, image optimization, and server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,43 @@
-import { defineConfig } from 'vite';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['playboy-3q2r.onrender.com']
-  }
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: ["playboy-3q2r.onrender.com"]
+      },
+      plugins: [
+        react(),
+        ViteImageOptimizer({
+          png: {
+            quality: 80,
+          },
+          jpeg: {
+            quality: 80,
+          },
+          jpg: {
+            quality: 80,
+          },
+          webp: {
+            quality: 80,
+          },
+          include: ['**/*.{png,jpg,jpeg,webp}'],
+          exclude: ['node_modules/**'],
+        }),
+      ],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
 });
